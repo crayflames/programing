5680ff7e82c4778269823f91f439189cad9c0e4a
RogerHU <crayflames@gmail.com>
2017-02-07 15:21:29 +0800

    Update subprocess_usage.py

--- python/script_format/subprocess_usage.py
+++ python/script_format/subprocess_usage.py
@@ -1,7 +1,9 @@
 #! /usr/bin/env python3
 # -*- coding: utf8 -*-
 import subprocess
+#執行程式用
 subprocess.call(['ethtool', '-S', 'enp0s25'])
+subprocess.check_call(['ethtool', '-S', 'enp0s25'])
+#要將輸出結果存進變數
 s=subprocess.getoutput('cat /sys/class/net/enp0s25/operstate')
 print(s)
-subprocess.check_call(['ethtool', '-S', 'enp0s25'])
