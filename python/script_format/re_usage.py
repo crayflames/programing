2a8a99556f3bb4d90a2e591a652869d39d7fca75
RogerHU <crayflames@gmail.com>
2017-02-07 15:22:55 +0800

    Update re_usage.py

--- python/script_format/re_usage.py
+++ python/script_format/re_usage.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 import re
+#取代字串  類似sed
 text= 'one , two , three'
 word='one'
 cmdRt=re.sub(word, '123' , text)
 print (cmdRt)
 
+#比對輸入格式符合macaddress \w 接受數字及英文大小寫
 x=input('input => ')
 print(x)
 m = re.match(r'\w\w:\w\w:\w\w:\w\w:\w\w:\w\w',x)
