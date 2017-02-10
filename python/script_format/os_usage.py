1de3b28c5793c1072602069cf4ed9fc7f672e08a
RogerHU <crayflames@gmail.com>
2017-02-10 14:45:53 +0800

    Update os_usage.py

--- python/script_format/os_usage.py
+++ python/script_format/os_usage.py
@@ -3,8 +3,15 @@
 import sys
 import os
 import stat
+#列出資料夾底下的檔案，清單形式
 os.listdir()
+#修改檔案目錄
 os.chmod('file',stat.S_IWOTH)
+#檔案存在
 os.path.exists('/sys/class/net/eth0')
+#切換路徑
 os.chdir(path)
+#是否為檔案
 os.path.isfile('file')
+#回傳當前目錄
+os.getcwd()
