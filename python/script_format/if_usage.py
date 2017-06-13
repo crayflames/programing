6ea5eada965c11fbfceb45226c8ffcb9c8e12979
crayflames <crayflames@gmail.com>
2017-06-13 11:12:29 +0800

    0613_if

--- python/script_format/if_usage.py
+++ python/script_format/if_usage.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+#This block is if not
 import os
 cmd = '/home/roger/Desktop'
+
+if os.path.exists(cmd + '/test'):
+	print("true")
+
 if not os.path.exists(cmd + '/test'):
 	# file not exist do this
 	print ("not exists")
@@ -14,3 +19,11 @@ if not cmdRt:
 else:
 	## output this because module is not load
 	print ('module not load')
+
+# this block is if is not:
+a=1
+b=2
+if a is not b:
+	print ('same')
+else:
+	print ('Not same')
--- python/script_format/ifisnot.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-a=1
-b=2
-if a is not b:
-	print ('same')
-else:
-	print ('Not same')
